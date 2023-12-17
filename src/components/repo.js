01bf61426e8b09b3f2598e721eb632574a10f19e
daniel <dev.danielcw@gmail.com>
2023-12-16 22:12:50 -0600

    repo grid layout built

--- src/components/repo.js
+++ src/components/repo.js
@@ -1,13 +1,40 @@
-import { Card } from "@mui/material"
+import { Card, Box, Typography, CardContent, CardActions, Grid } from "@mui/material"
 
 const Repo = (data) => {
     return (
-        <Card>
-            <p>name: {data?.name ?? "err"}</p>
-            <p>language: {data?.langauge ?? "err"}</p>
-            <p>forks: {data?.forks ?? "err"}</p>
-            <p>login: {data?.owner.login ?? "err"}</p>
-        </Card>
+            <Box sx={{
+                borderRadius: "5px",
+                padding: "10px 0px",
+                background: "#efefef"
+            }}>
+                <Typography variant="h5" component="h5" sx={{ margin: "10px 0px"}}>
+                    {/* name: {data?.name ?? "err"} */}
+                    Auth App
+                </Typography>
+                <Box sx={{display: "flex", justifyContent: "center", gap: "20px"}}>
+                    <Box sx={{ display: "flex", justifyContent: "center", flexDirection: "column"}}>
+                        <Typography variant="body" component="p">Language</Typography>
+                        <Typography variant="body2" component="p">{data?.langauge ?? "err"}</Typography>
+                    </Box>
+                    <Box sx={{ display: "flex", justifyContent: "center", flexDirection: "column"}}>
+                        <Typography variant="body" component="p">Forks</Typography>
+                        <Typography variant="body2" component="p">{data?.forks ?? "err"}</Typography>
+                    </Box>
+                    <Box sx={{ display: "flex", justifyContent: "center", flexDirection: "column"}}>
+                        <Typography variant="body" component="p">Owner</Typography>
+                        <Typography variant="body2" component="p">{data?.owner ?? "err"}</Typography>
+                    </Box>                   
+                </Box>
+                    {/* card actions dropdown for project description? */}
+                    {/* links to demo and code */}
+                    {/*              
+                        <p>language: {data?.langauge ?? "err"}</p>
+                        <p>forks: {data?.forks ?? "err"}</p>
+                        <p>login: {data?.owner?.login ?? "err"}</p> */
+                    }                    
+            </Box>
+
+
     )
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,10 @@
-import { Button, Container } from '@mui/material';
+import { Button, Container, Box } from '@mui/material';
 import '../App.css';
 import { useAuth0 } from '@auth0/auth0-react';
-import sample from "../images/sample.JPG"
 import { useEffect, useState } from 'react';
 import { fetchUser } from '../hooks/fetchUser';
 import { fetchRepos } from '../hooks/fetchProjects';
+import Repo from "../components/repo";
 
 function App() {
 
@@ -14,9 +14,11 @@ function App() {
   
   const test = [
     {"test1": "test"},
+    {"test1": "test"},    
+    {"test1": "test"},    
+    {"test1": "test"},    
     {"test2": "test"}
   ]
-  const sadTest = null
 
   useEffect(() => {
     if(user) {
@@ -27,15 +29,34 @@ function App() {
     } else { return }
   }, [isAuthenticated])
 
+  const array = test.map(item => {
+    return <Repo />
+  })
   
   return (
     <div className="App">
-        <Container sx={{ border: 'solid red 1px'}}>
+      <Box sx={{ background: 'gray'}}>
+        <Container sx={{ padding: "50px 0px"}}>
             <img src={user?.picture ?? null} className="profilePic" />
             <h1> homepage hero </h1>
             <h2> Welcome, {user?.name ?? "guest"}! </h2>
+        </Container>        
+      </Box>
+
+        <Container sx={{
+          display: "grid",
+          gridGap: "20px",
+          padding: "10px 10px", 
+          gridTemplateColumns: {
+            xs: "repeat(1, minmax(100px, 1fr))",
+            md: "repeat(2, minmax(100px, 1fr))",
+            lg: "repeat(3, minmax(100px, 1fr))"
+          }
+        }}>
+            {isRepos ? array : <p>not awesome</p>}          
+
         </Container>
-        {isRepos ? <p>awesome</p> : <p>not awesome</p>}
+
     </div>
   );
 }
