4bf6a436fb10e947ed986008bdfb0877c5348f4a
daniel <dev.danielcw@gmail.com>
2023-12-17 01:28:48 -0600

    currently working through styling repo cards

--- src/components/repo.js
+++ src/components/repo.js
@@ -4,25 +4,28 @@ const Repo = (data) => {
     return (
             <Box sx={{
                 borderRadius: "5px",
-                padding: "10px 0px",
-                background: "#efefef"
+                background: "#efefef",
+                overflow: "hidden"
             }}>
-                <Typography variant="h5" component="h5" sx={{ margin: "10px 0px"}}>
-                    {/* name: {data?.name ?? "err"} */}
-                    Auth App
-                </Typography>
-                <Box sx={{display: "flex", justifyContent: "center", gap: "20px"}}>
+
+                <Typography 
+                    variant="h6" 
+                    component="h6" 
+                    sx={{ padding: "15px 10px", background: "red", textAlign: "left"}}
+                >{/* name: {data?.name ?? "err"} */}Auth App</Typography>
+                
+                <Box sx={{display: "flex", padding: "15px 10px", gap: "20px"}}>
                     <Box sx={{ display: "flex", justifyContent: "center", flexDirection: "column"}}>
                         <Typography variant="body" component="p">Language</Typography>
-                        <Typography variant="body2" component="p">{data?.langauge ?? "err"}</Typography>
+                        <Typography variant="body2" component="p">{data?.langauge ?? "0"}</Typography>
                     </Box>
                     <Box sx={{ display: "flex", justifyContent: "center", flexDirection: "column"}}>
                         <Typography variant="body" component="p">Forks</Typography>
-                        <Typography variant="body2" component="p">{data?.forks ?? "err"}</Typography>
+                        <Typography variant="body2" component="p">{data?.forks ?? "0"}</Typography>
                     </Box>
                     <Box sx={{ display: "flex", justifyContent: "center", flexDirection: "column"}}>
                         <Typography variant="body" component="p">Owner</Typography>
-                        <Typography variant="body2" component="p">{data?.owner ?? "err"}</Typography>
+                        <Typography variant="body2" component="p">{data?.owner ?? "danielCW7"}</Typography>
                     </Box>                   
                 </Box>
                     {/* card actions dropdown for project description? */}
